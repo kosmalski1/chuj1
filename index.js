5e196a8a0dcb1b6af6be0147b0376419c3b763a6
kosmalski1 <pawelekk0505@gmail.com>
2023-10-11 14:01:18 +0200

    kurwa dziala

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const mongostring = "mongodb+srv://guwnomajster:1234@cluster0.kqlzt4t.mongodb.net/?retryWrites=true&w=majority";
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(mongostring);
-async function main(){
-    await client.connect();
-    await client.close();
+async function main() {
+    try { 
+        await client.connect()
+        await listDB(client)
+    } catch (error) {
+        console.error(error)
+    } finally {
+
+
+        await client.close()
+    }
+}
+async function listDB(client) {
+    let databaselist = await client.db().admin().listDatabases()
+   // console.log(databaselist)
+   databaselist.databases.forEach(database => {
+    console.log("nazwa: "+ database.name + ", Rozmiar" + database.sizeOnDisk)
+    
+   });
 }
 main();
