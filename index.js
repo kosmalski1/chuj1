0e0ffc53f739199d2d2c0b5027415d3b6fc1d300
kosmalski1 <pawelekk0505@gmail.com>
2023-11-08 14:04:03 +0100

    rzygi

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const mongostring = "mongodb+srv://guwnomajster:1234@cluster0.kqlzt4t.mongodb.net/?retryWrites=true&w=majority";
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(mongostring);
-async function main() {
-    try { 
-        await client.connect()
-        await listDB(client)
-    } catch (error) {
-        console.error(error)
-    } finally {
+const express = require('express');
+//przygotuj instancję serwera http
+const app = express();
+//zaimportuj nasze funkcje do danych
+const db = require('./db');
 
+//zdefiniuj ścieżkę dla głownego "folderu" aplikacji przy użyciu żądania GET
+//używamy funkcji asynchronicznej pod przyszłe funkcje mongodb
+app.get('/', async (req, res) => {
+    res.send("ładzia!");
+})
 
-        await client.close()
-    }
-}
-async function listDB(client) {
-    let databaselist = await client.db().admin().listDatabases()
-   // console.log(databaselist)
-   databaselist.databases.forEach(database => {
-    console.log("nazwa: "+ database.name + ", Rozmiar" + database.sizeOnDisk)
-    
-   });
-}
-main();
+//funkcja wyświetla wszystkie rekordy z bazy danych
+app.get('/listAll', async (req, res) => {
+    res.write("<h1>Lista wszystkich rekordów w bazie</h1>");
+    const client = await db.connect();
+    res.write("<table>");
+    let list = await db.getAllListings(client);
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url + "</td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("</tr>");
+    });
+    res.write("</table>");
+    db.close(client);
+    res.end();
+})
+
+//uruchom serwer
+app.listen(8000);
